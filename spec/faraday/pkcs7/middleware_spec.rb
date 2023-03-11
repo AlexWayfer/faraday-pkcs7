40c84c68ca2bf20a28129d899deab4dc24e42eba
Alexander Popov <alex.wayfer@gmail.com>
2023-03-11 18:24:56 +0300

    OK, just disable the cop instead of trying to resolve the offense
    https://github.com/rubocop/rubocop-rspec/pull/1566#issuecomment-1464933825

--- spec/faraday/pkcs7/middleware_spec.rb
+++ spec/faraday/pkcs7/middleware_spec.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe Faraday::PKCS7::Middleware do
+  # rubocop:disable RSpec/PendingWithoutReason
   ## https://github.com/rubocop/rubocop-rspec/pull/1566#issuecomment-1464932671
-  it 'does something' do
-    pending 'write some tests specific to the middleware'
-  end
+  ## https://github.com/rubocop/rubocop-rspec/pull/1566#issuecomment-1464933825
+  pending 'write some tests specific to the middleware'
+  # rubocop:enable RSpec/PendingWithoutReason
 end
