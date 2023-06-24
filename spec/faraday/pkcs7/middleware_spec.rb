f22b467a0b632b3fac05dd4603a7e75e38f89983
Alexander Popov <alex.wayfer@gmail.com>
2023-06-24 21:31:19 +0300

    Resolve new RuboCop RSpec offense

--- spec/faraday/pkcs7/middleware_spec.rb
+++ spec/faraday/pkcs7/middleware_spec.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe Faraday::PKCS7::Middleware do
-  # rubocop:disable RSpec/PendingWithoutReason
-  ## https://github.com/rubocop/rubocop-rspec/pull/1566#issuecomment-1464932671
-  ## https://github.com/rubocop/rubocop-rspec/pull/1566#issuecomment-1464933825
   pending 'write some tests specific to the middleware'
-  # rubocop:enable RSpec/PendingWithoutReason
 end
